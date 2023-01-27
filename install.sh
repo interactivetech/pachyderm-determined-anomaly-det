e24fd43253a599a0b6ed3519b2f7262698ab9621
Andrew Mendez <andrew.mendez@hpe.com>
2023-01-27 14:45:47 -0500

    update

--- install.sh
+++ install.sh
@@ -10,3 +10,4 @@ pip install argparse_formatter
 pip install terminaltables
 pip install pyyaml
 pip install streamlit
+pip install torch==1.11.0+cu113 torchvision==0.12.0+cu113 torchaudio==0.11.0 --extra-index-url https://download.pytorch.org/whl/cu113
