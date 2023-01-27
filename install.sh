dd9a6d6c878d957322a9771002fd91b0e3464650
Andrew Mendez <andrew.mendez@hpe.com>
2023-01-27 14:51:31 -0500

    update

--- install.sh
+++ install.sh
@@ -11,3 +11,4 @@ pip install terminaltables
 pip install pyyaml
 pip install streamlit
 pip install torch==1.11.0+cu113 torchvision==0.12.0+cu113 torchaudio==0.11.0 --extra-index-url https://download.pytorch.org/whl/cu113
+pip install scikit-learn
